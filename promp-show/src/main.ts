ec2e676ca61a99e9805d03305a9b7c6ee0682315
Magma-Flavio Portela <fmspx@hotmail.com>
2021-12-15 19:25:56 -0300

    typescript demos

--- promp-show/src/main.ts
+++ promp-show/src/main.ts
@@ -1,6 +1,7 @@
 //reference
 //https://www.npmjs.com/package/prompts
-async function showOption(){
+async function showSinglePrompt(){
+    //Prompt with a single prompt object. Returns an object with the response.
     const prompts = require('prompts');
     const response = await prompts({
         type: 'text',
@@ -11,8 +12,58 @@ async function showOption(){
     console.log(`Olá ${response.value}`);
 };
 
+async function showPromptChain(){
+    //Prompt with a list of prompt objects. Returns an object with the responses. Make sure to give each prompt a unique name property to prevent overwriting values.
+    const prompts = require('prompts');
+    const questions = [
+    {
+        type: 'text',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'number',
+        name: 'age',
+        message: 'How old are you?'
+    },
+    {
+        type: 'text',
+        name: 'about',
+        message: 'Tell something about yourself',
+        initial: 'Why should I?'
+    }
+    ];
+
+    (async () => {
+    const response = await prompts(questions);
+
+    // => response => { username, age, about }
+    })();
+}
+
+async function showDynamicPrompts(){
+    //Prompt properties can be functions too. Prompt Objects with type set to falsy values are skipped.
+    const prompts = require('prompts');
+    const questions = [
+        {
+            type: 'text',
+            name: 'dish',
+            message: 'Do you like pizza?'
+        },
+        {
+            type: prev => prev == 'pizza' ? 'text' : null,
+            name: 'topping',
+            message: 'Name a topping'
+        }
+    ];
+
+    (async () => {
+    const response = await prompts(questions);
+    })();
+}
+
 async function start(){
-    showOption();
+    showSinglePrompt();
 }
 
 start();
