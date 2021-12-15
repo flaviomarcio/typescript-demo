e001ce7e277734d058ed12f0daddcd6f6e2434a5
Magma-Flavio Portela <fmspx@hotmail.com>
2021-12-15 19:13:16 -0300

    typescript demos

---
+++ promp-show/src/main.ts
@@ -0,0 +1,16 @@
+async function showOption(){
+    const prompts = require('prompts');
+    const response = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Qual seu nome?',
+        validate: value => value!==''
+    });    
+    console.log(`Olá ${response.value}`);
+};
+
+async function start(){
+    showOption();
+}
+
+start();
