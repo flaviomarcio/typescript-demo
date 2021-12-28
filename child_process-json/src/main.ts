3e680710a691ccc88a5742b0de3f9041c41406b2
Magma-Flavio Portela <fmspx@hotmail.com>
2021-12-27 21:21:44 -0300

    repository

--- child_process-json/src/main.ts
+++ child_process-json/src/main.ts
@@ -1,8 +1,16 @@
 //reference
 //https://nodejs.org/api/child_process.html
 
-function list(){
-    const { exec } = require("child_process");    
+const { 
+    spawn,
+    spawnSync, 
+    exec,
+    execSync, 
+    execFileSync,
+  } = require('child_process');
+
+
+function call_exec(){    
     exec("ls -l | jq -R '[.]' | jq -s -c 'add'", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -12,18 +20,54 @@ function list(){
             console.log(`stderr: ${stderr}`);
             return;
         }
-        let outputList=JSON.parse(stdout);
-        for(let row of outputList){
-            console.log(row)
-        }
-        return [];
+        console.log(JSON.parse(stdout))
     });
     return [];
 }
 
+function call_execSync(){    
+    const response=execSync("ls -l | jq -R '[.]' | jq -s -c 'add'").toString();
+    return JSON.parse(response);
+}
+
+function call_spawn(){    
+    const ls = spawn('ls', ['-l', '*']);    
+    ls.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    
+    ls.on('close', (code) => {
+      console.log(`child process close all stdio with code ${code}`);
+    });
+    
+    ls.on('exit', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+
+}
+
+function call_spawnSync(){    
+    // const ls = spawnSync('ls', ['-l', '*']);    
+    // ls.stdout.on('data', (data) => {
+    //   console.log(`stdout: ${data}`);
+    // });
+    
+    // ls.on('close', (code) => {
+    //   console.log(`child process close all stdio with code ${code}`);
+    // });
+    
+    // ls.on('exit', (code) => {
+    //   console.log(`child process exited with code ${code}`);
+    // });
+
+}
+
 
 function start(){
-    list()
+    // call_exec()
+    // console.log(call_execSync());
+    // call_spawn()
+    console.log(call_spawnSync());    
 }
 
 start();
